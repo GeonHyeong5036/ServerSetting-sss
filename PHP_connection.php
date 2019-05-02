d2762eb6de814c66ae95ea40efd1272ba0023ab4
KGH <show11837@gamil.com>
2019-05-02 23:57:57 +0900

    Update PHP_connection.php

--- PHP_connection.php
+++ PHP_connection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("15.164.89.139","root","super");
+$con=mysqli_connect("localhost","root","super");
 
 if(mysqli_connect_errno($con)){
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -7,13 +7,13 @@ if(mysqli_connect_errno($con)){
 
 mysqli_set_charset($con, "utf8");
 
-$res = mysqli_query($con, "select * from users");
+$res = mysqli_query($con, "select * from test");
 
 $result = array();
 
 while($row = mysqli_fetch_array($res)){
 
-  array_push($result, array('id'=>$row[0], 'email'=>$row[1], 'passward'=>$row[2], 'name'=>$row[3], 'school'=>$row[4]));
+  array_push($result, array('id'=>$row[0], 'name'=>$row[1], 'address'=>$row[2]));
 }
 echo json_encode(array("result"=>$result));
 mysqli_close($con);
