92a56771c304f3ea3c8a760671e2683074c19dbc
KGH <show11837@gamil.com>
2019-05-03 01:08:04 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,18 +2,13 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../includes/DbConnect.php';
+require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    $db = new DbConnect;
 
-    if($db->connect() != null){
-      echo 'Connection Successfull';
-    }
+    $response->getBody()->write("Hello, $name");
 
     return $response;
 });
