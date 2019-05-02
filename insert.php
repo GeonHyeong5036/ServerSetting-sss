b4da842a4ebfc55460f937a9a4f3938dd2ee49e7
KGH <show11837@gamil.com>
2019-05-03 01:14:09 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,9 +2,10 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
 
