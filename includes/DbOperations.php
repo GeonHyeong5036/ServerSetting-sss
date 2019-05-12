5ddadc5af5a6705688c83a3f3058f09cd7e2ac71
KGH <show11837@gamil.com>
2019-05-12 14:30:11 +0900

    Update DbOperations.php

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -9,7 +9,7 @@
     }
 
     public function createUser($kakaoId, $name, $member){
-      if(!$this->isKakaoIdExist($kakaoId)){
+      if(!$this->isKakaoIdExist($kakaoId, $member)){
         $stmt = $this->con->prepare("INSERT into users (kakaoId, name, member) values (?, ?, ?)");
         $stmt->bind_param("sss", $kakaoId, $name, $member);
         if($stmt->execute()){
@@ -21,12 +21,37 @@
       return USER_EXISTS;
     }
 
-    private function isKakaoIdExist($kakaoId){
-      $stmt = $this->con->prepare("SELECT id from users where (kakaoId = ? and member = 1)");
+    private function isKakaoIdExist($kakaoId, $member){
+      if($member == '1'){
+        return isMemberAlready($kakaoId);
+      }else{
+        $stmt = $this->con->prepare("SELECT id from users where (kakaoId = ? and member = 1)");
+        $stmt->bind_param("s", $kakaoId);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt->num_rows > 0;
+      }
+    }
+
+    private function isMemberAlready($kakaoId){
+      $stmt = $this->con->prepare("SELECT id from users where (kakaoId = ? and member = 0)");
       $stmt->bind_param("s", $kakaoId);
       $stmt->execute();
       $stmt->store_result();
-      return $stmt->num_rows > 0;
+      if($stmt->num_rows > 0){
+        $stmt = $this->con->prepare("UPDATE users SET member = 1 WHERE kakaoId = ?");
+        $stmt->bind_param("s", $kakaoId);
+        if($stmt->execute()){
+          echo "업데이트 성공";
+          return true;
+          }
+        else{
+          echo "업데이트 실패"
+          return true;
+        }
+      }
+      return false;
+
     }
 
     private function getIdByKakaoId($kakaoId){
@@ -39,10 +64,11 @@
     }
 
     public function getUser($kakaoId){
-        $stmt = $this->con->prepare("SELECT id, kakaoId, name, member FROM users where kakaoId = ?;");
+        $stmt = $this->con->prepare("SELECT id, kakaoId, name, member FROM users where kakaoId = ?");
         $stmt->bind_param("s", $kakaoId);
         $stmt->execute();
         $stmt->bind_result($id, $kakaoId, $name, $member);
+        $stmt->fetch();
         $user = array();
         $user['id'] = $id;
         $user['kakaoId']=$kakaoId;
