67af729d4780aef8336d997ee0ad38321f117f83
KGH <show11837@gamil.com>
2019-05-20 03:52:35 +0900

    Update DbAnalysis.php
    Change loacation of var
    global var : $availableMeetingTimes

--- includes/DbAnalysis.php
+++ includes/DbAnalysis.php
@@ -1,9 +1,10 @@
 <?php
   class DbAnalysis{
-    private $availableMeetingTimes = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39);
     private $con;
-    private $index=-1;
+    private $availableMeetingTimes = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39);
     private $targetCellPositionList = array();
+    private $index=-1;
+
 
     function __construct(){
       require_once dirname(__FILE__) . '/DbConnect.php';
@@ -12,7 +13,8 @@
     }
 
     public function getAvailableMeetingTimes($array){
-      global $targetCellPositionList, $availableMeetingTimes;
+      global $targetCellPositionList;
+      global $availableMeetingTimes;
 
       foreach ($array as $kakaoid) {
         $this->getUnAvailableCellPostion($kakaoid);
