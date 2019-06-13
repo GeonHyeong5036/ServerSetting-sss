86cdd31e53b4d4c1e67cec756637ad0362b4b3be
KGH <show11837@gmail.com>
2019-06-13 13:04:12 +0900

    Update DbAnalysis.php

--- includes/DbAnalysis.php
+++ includes/DbAnalysis.php
@@ -83,10 +83,12 @@
           $cellListByday = array();
           for($cell = $time; $cell < $time+5 ; $cell++){
             $cellListByday[$cell] = $availableMeetingTimes[$cell];
+            echo $cell . " \n";
           }
           $minInt = min($cellListByday);
           $filter_cellListByday = preg_grep("/^$minInt/i", $cellListByday);
           $filter_cellListByday = array_keys($filter_cellListByday);
+          return $filter_cellListByday;
           $filter_availableMeetingTimes = array_merge($filter_availableMeetingTimes, $filter_cellListByday);
         }
         sort($filter_availableMeetingTimes);
